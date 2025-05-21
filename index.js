f95a3709f3bbba7c917d2e1851d95d0cbb54913e
itay-explorium <itay.gutman@explorium.ai>
2025-05-21 14:56:06 +0300

    fix credentials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This file ensures n8n can find and load your nodes and credentials
 const { ExploriumMcp } = require('./dist/nodes/ExploriumMcp/ExploriumMcp.node.js');
-const { HttpHeaderAuth } = require('./dist/credentials/HttpHeaderAuth.js');
+const { HttpHeaderAuth } = require('./dist/credentials/HttpHeaderAuth.credentials.js');
 
 module.exports = {
 	nodeTypes: { exploriumMcp: ExploriumMcp },
